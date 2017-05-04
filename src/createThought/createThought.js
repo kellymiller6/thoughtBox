b8c3c92756546ffbc772519515ab352d5e85ddd4
Kelly Miller <kellyannemiller@gmail.com>
2017-05-04 09:37:44 -0600

    start

--- src/createThought/createThought.js
+++ src/createThought/createThought.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 export default class CreateThought extends Component {
+  constructor(){
+    super()
+    this.state = {
+      body: '',
+      title:''
+    }
+  }
   render() {
     return(
-      <div>CreateThought</div>
+      <div>
+        <h1>CreateThought</h1>
+        <input value={ this.state.title } onChange={(e) => this.setState({ title: e.target.value })} />
+        <input value={ this.state.body } onChange={(e) => this.setState({ body: e.target.value })}/>
+        <button>Submit</button>
+      </div>
     );
   }
 
